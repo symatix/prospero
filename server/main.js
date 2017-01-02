f8834da596cebbbf8c7c6d1ee44990aecec333f3
Dino Kraljeta <Dino Kraljeta>
2017-01-02 14:16:23 +0100

    deploy to ubuntu server

--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-
+	//if(!Meteor.users.find()){
+		Accounts.createUser({
+		    	username:"admin",
+		        password: "admin",
+		        profile:{
+					visible:0,
+		        	full_name: "admin",
+		        	email: "admin@prospero.com",
+		        	institution: "Prospero",
+		       		telephone: "091 / 555 - 007"
+		        }
+		});
+	//}
 });
